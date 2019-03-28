69e4ea4622b5045ca600e69b857e8d934a73271f
Spinking <spinking@mail.ru>
2019-03-28 14:38:01 +0400

    add SettingsActivity

--- app/src/main/java/studio/eyesthetics/soundrecorder/activities/SettingsActivity.java
+++ app/src/main/java/studio/eyesthetics/soundrecorder/activities/SettingsActivity.java
@@ -2,11 +2,31 @@ package studio.eyesthetics.soundrecorder.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+
+import studio.eyesthetics.soundrecorder.R;
 
 public class SettingsActivity extends android.support.v7.app.AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setTitle(R.string.action_settings);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new SettingsFragment())
+                .commit();
     }
 }
