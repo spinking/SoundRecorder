0c8cb4b858001ba9bcc968462e5cb6d162c298d8
Spinking <spinking@mail.ru>
2019-03-28 16:27:53 +0400

    add fragment_license desc

--- app/src/main/java/studio/eyesthetics/soundrecorder/fragments/LicensesFragment.java
+++ app/src/main/java/studio/eyesthetics/soundrecorder/fragments/LicensesFragment.java
@@ -1,19 +1,22 @@
 package studio.eyesthetics.soundrecorder.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import studio.eyesthetics.soundrecorder.R;
 
 public class LicensesFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         LayoutInflater dialogInflater = getActivity().getLayoutInflater();
-        View openSourceLicensesview = dialogInflater.inflate(R.layout.fragment_licenses, null);
+        View openSourceLicensesView = dialogInflater.inflate(R.layout.fragment_licenses, null);
 
-        AlertDialog.Bulider dialogBuilder = new AlertDialog.Builder(getActivity());
-        dialogBuilder.setView(openSourceLicensesview)
-                .setTitle(getString(R.string.dialog_title_licenses)))
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+        dialogBuilder.setView(openSourceLicensesView)
+                .setTitle(getString(R.string.dialog_title_licenses))
                 .setNeutralButton(android.R.string.ok, null);
 
         return dialogBuilder.create();
