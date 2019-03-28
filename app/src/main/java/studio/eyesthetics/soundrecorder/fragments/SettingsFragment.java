00ed2cd958d5d168cd160663dca8e3ed9a749171
Spinking <spinking@mail.ru>
2019-03-28 15:43:10 +0400

    add SettingsFragment

--- app/src/main/java/studio/eyesthetics/soundrecorder/fragments/SettingsFragment.java
+++ app/src/main/java/studio/eyesthetics/soundrecorder/fragments/SettingsFragment.java
@@ -2,10 +2,14 @@ package studio.eyesthetics.soundrecorder.fragments;
 
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+
+import studio.eyesthetics.soundrecorder.BuildConfig;
 import studio.eyesthetics.soundrecorder.R;
 import studio.eyesthetics.soundrecorder.MySharedPreferences;
+import studio.eyesthetics.soundrecorder.activities.SettingsActivity;
 
 
 public class SettingsFragment extends PreferenceFragment {
@@ -16,5 +20,23 @@ public class SettingsFragment extends PreferenceFragment {
 
         CheckBoxPreference highQualityPref = (CheckBoxPreference) findPreference(getResources().getString(R.string.pref_high_quality_key));
         highQualityPref.setChecked(MySharedPreferences.getPrefHighQuality(getActivity()));
+        highQualityPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                MySharedPreferences.setPrefHighQuality(getActivity(), (boolean) newValue);
+                return true;
+            }
+        });
+
+        Preference aboutPref = findPreference(getString(R.string.pref_about_key));
+        aboutPref.setSummary(getString(R.string.pref_about_desc, BuildConfig.VERSION_NAME));
+        aboutPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                LicensesFragment licensesFragment = new LicensesFragment();
+                licensesFragment.show(((SettingsActivity)getActivity()).getSupportFragmentManager().beginTransaction(), "dialog_licenses");
+                return true;
+            }
+        });
     }
 }
