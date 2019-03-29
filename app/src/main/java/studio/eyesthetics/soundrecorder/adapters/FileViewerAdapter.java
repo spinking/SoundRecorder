d621804d4c828cdcd64e616c9fb0a8220b6175cf
Spinking <spinking@mail.ru>
2019-03-29 10:19:30 +0400

    add DBHelper

--- app/src/main/java/studio/eyesthetics/soundrecorder/adapters/FileViewerAdapter.java
+++ app/src/main/java/studio/eyesthetics/soundrecorder/adapters/FileViewerAdapter.java
@@ -1,8 +1,10 @@
 package studio.eyesthetics.soundrecorder.adapters;
 
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ViewHolder;
+import studio.eyesthetics.soundrecorder.listeners.OnDatabaseChangedListener;
 
 public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter>.RecordingsViewHolder implements OnDatabaseChangedListener {
+    private static final String LOG_TAG = "FileViewerAdapter";
 
+    private DBHelper mDatabase;
 }
