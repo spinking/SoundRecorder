405d4691189fae557630c2f377911623f818522c
Spinking <spinking@mail.ru>
2019-03-29 11:22:46 +0400

    add card_view layout

--- app/src/main/java/studio/eyesthetics/soundrecorder/adapters/FileViewerAdapter.java
+++ app/src/main/java/studio/eyesthetics/soundrecorder/adapters/FileViewerAdapter.java
@@ -1,10 +1,52 @@
 package studio.eyesthetics.soundrecorder.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import studio.eyesthetics.soundrecorder.DBHelper;
+import studio.eyesthetics.soundrecorder.RecordingItem;
 import studio.eyesthetics.soundrecorder.listeners.OnDatabaseChangedListener;
 
-public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter>.RecordingsViewHolder implements OnDatabaseChangedListener {
+public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.RecordingsViewHolder> implements OnDatabaseChangedListener {
     private static final String LOG_TAG = "FileViewerAdapter";
 
     private DBHelper mDatabase;
+
+    RecordingItem item;
+    Context mContext;
+    LinearLayoutManager lim;
+
+    public FileViewerAdapter(Context context, LinearLayoutManager linearLayoutManager) {
+        super();
+        mContext = context;
+        mDatabase = new DBHelper(mContext);
+        mDatabase.setOnDatabaseChangedListener(this);
+        lim = linearLayoutManager;
+    }
+
+    @Override
+    public void onBindViewHolder(final RecordingsViewHolder holder, int position) {
+
+    }
+
+
+
+    public static class RecordingsViewHolder extends RecyclerView.ViewHolder {
+        protected TextView vName;
+        protected TextView vLength;
+        protected TextView vDateAdded;
+        protected View cardView;
+
+        public RecordingsViewHolder(View v) {
+            super(v);
+            vName = (TextView) v.findViewById(R.id.file_name_text);
+            vLength = (TextView) v.findViewById(R.id.file_length_text);
+            vDateAdded = (TextView) v.findViewById(R.id.file_date_added_text);
+            cardView = v.findViewById(R.id.card_view);
+        }
+    }
+
 }
